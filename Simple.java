baef5555e49aff2d521db55cb911646b406510eb
tarun arora <tarun.arora@live.in>
2020-10-19 13:19:57 +0530

    comitted the changes tarora

--- Simple.java
+++ Simple.java
@@ -3,7 +3,7 @@ public class Simple {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method 
-		System.out.println("hello updated");
+		System.out.println("hello updated again");
 
 	}
 
